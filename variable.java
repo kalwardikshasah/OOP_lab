ae2c70ae3d45594a226015cbade52cf54f6f9507
kalwardikshasah <kalwardikshasah@gmail.com>
2026-01-15 18:17:00 +0545

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,13 @@
+public class variable {
+    public static void main(String[] args) {
+        // Variables
+        String name = "Diya";
+        int age = 30;
+        String neighbour = "Siya";
+        String friend = neighbour;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(friend);
+    }
+}
