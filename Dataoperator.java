786a7fcfe5bfa457d625453a6c6d7d0453b79eea
kalwardikshasah <kalwardikshasah@gmail.com>
2026-01-16 22:00:19 +0545

    Add files via upload

---
+++ Dataoperator.java
@@ -0,0 +1,12 @@
+public class Dataoperator {
+    public static void main(String[] args) {
+         int a = 68;
+        int b = 22;
+
+        System.out.println(a + b); 
+        System.out.println(a - b); 
+        System.out.println(a * b); 
+        System.out.println(a / b); 
+        System.out.println(a % b);
+    }
+}
